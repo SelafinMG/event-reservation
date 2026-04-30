6b26cd83309321b67cc1ccde5c926663704697d8
Yoan <yoandiny14@gmail.com>
2026-05-01 02:24:07 +0300

    added session verification and http status code

--- backend/controllers/session.controller.js
+++ backend/controllers/session.controller.js
@@ -3,6 +3,12 @@ import { getSessionQuestionsService } from "../services/session.service.js";
 
 export const getSessionQuestionsController = async(req, res) => {
     const { sessionId } = req.params;
+    if(!sessionId) {
+        return res.status(400).json({ error: "Session ID is required" });
+    }
     const questions = await getSessionQuestionsService(sessionId);
-    res.json(questions);
+    if(!questions) {
+        return res.status(404).json({ error: "Session not found" });
+    }
+    return res.status(200).json(questions);
 }
--- backend/services/session.service.js
+++ backend/services/session.service.js
@@ -2,6 +2,11 @@ import pool from "../config/pg.js";
 
 export const getSessionQuestionsService = async (sessionId) => {
     try {
+        const sessionQuery = "SELECT * FROM sessions WHERE id = $1";
+        const sessionRequest = await pool.query(sessionQuery, [sessionId]);
+        if(sessionRequest.rowCount === 0) {
+            throw new Error("Session not found");
+        }
         const getQUestionsQuery = "SELECT * FROM questions WHERE session_id = $1";
         const questionRequest = await pool.query(getQUestionsQuery, [sessionId]);
         return questionRequest.rows;
